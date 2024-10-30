831216ba869f2a130e25dd708d88f3ac0fa372a6
Benjamin Borbe <benjamin.borbe@gmail.com>
2024-10-30 15:21:07 +0100

    fix NewServerTLS

--- server.go
+++ server.go
@@ -6,7 +6,6 @@ package http
 
 import (
 	"context"
-	"crypto/tls"
 	"fmt"
 	"net/http"
 
@@ -23,29 +22,12 @@ func NewServerWithPort(port int, router http.Handler) run.Func {
 }
 
 func NewServer(addr string, router http.Handler) run.Func {
-	return newServerTLS(addr, router, nil)
-}
-
-func NewServerTLS(addr string, router http.Handler, serverCertPath string, serverKeyPath string) run.Func {
-	return func(ctx context.Context) error {
-		cert, err := tls.LoadX509KeyPair(serverCertPath, serverKeyPath)
-		if err != nil {
-			return errors.Wrapf(ctx, err, "load server certificate and key failed")
-		}
-		tlsConfig := &tls.Config{
-			Certificates: []tls.Certificate{cert},
-		}
-		return newServerTLS(addr, router, tlsConfig).Run(ctx)
-	}
-}
-
-func newServerTLS(addr string, router http.Handler, tlsConfig *tls.Config) run.Func {
 	return func(ctx context.Context) error {
 
 		server := &http.Server{
 			Addr:      addr,
 			Handler:   router,
-			TLSConfig: tlsConfig,
+			TLSConfig: nil,
 		}
 		go func() {
 			select {
@@ -56,6 +38,29 @@ func newServerTLS(addr string, router http.Handler, tlsConfig *tls.Config) run.F
 			}
 		}()
 		err := server.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			glog.V(0).Info(err)
+			return nil
+		}
+		return errors.Wrapf(ctx, err, "httpServer failed")
+	}
+}
+
+func NewServerTLS(addr string, router http.Handler, serverCertPath string, serverKeyPath string) run.Func {
+	return func(ctx context.Context) error {
+		server := &http.Server{
+			Addr:    addr,
+			Handler: router,
+		}
+		go func() {
+			select {
+			case <-ctx.Done():
+				if err := server.Shutdown(ctx); err != nil {
+					glog.Warningf("shutdown failed: %v", err)
+				}
+			}
+		}()
+		err := server.ListenAndServeTLS(serverCertPath, serverKeyPath)
 		if errors.Is(err, http.ErrServerClosed) {
 			glog.V(0).Info(err)
 			return nil
